980743c4d259da2cd9e96ccd85f493834195ae7d
csammat1 <csammat1@binghamton.edu>
2025-01-23 14:10:23 -0500

    added 'global' variables for use within loops

--- src/minesweeper.cpp
+++ src/minesweeper.cpp
@@ -5,11 +5,16 @@
 using namespace std;
 
 void menu_loop(string &state){
-    
+
 }
 
-int main(){
+int main(){    
+    //set up "global" variables
     string state = "MENU";
+
+    float tiles_width = 10;
+    float tiles_height = 10;
+    float bomb_ratio = .10;
 
     while(true){
         if(state == "Menu"){
