a0ae709e572e9ac34846734c357a8f7fb482e86f
csammat1 <csammat1@binghamton.edu>
2025-01-22 20:08:19 -0500

    small edits to cell.h and got WSL with vs code working

--- src/Cell.h
+++ src/Cell.h
@@ -3,8 +3,15 @@
 
 class Cell{
     public:
-
+        int get_content();
+        bool get_revealed();
+        void set_content(int i);
+        void reveal();
     private:
+
+    int content;
+    bool revealed;
+    const sf::Texture* texture; // Pointer to texture
 };
 
 
