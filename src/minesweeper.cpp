425534a822ccc2e6c0f122ce9650f969fe012fe4
csammat1 <csammat1@binghamton.edu>
2025-02-02 14:09:18 -0500

    added Board class to remove excess initiation from within main. Changed print board to display bombs as B instead of 9

--- src/minesweeper.cpp
+++ src/minesweeper.cpp
@@ -2,84 +2,23 @@
 
 #include <stdlib.h>
 #include <vector>
-#include <algorithm>
 #include <cstdlib>
-#include <random>
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "Cell.h"
+#include "Board.h"
 using namespace std;
-
-void reset_board(int rows, int cols, vector<vector<Cell>>& board){
-    for (int i = 0; i < rows; i++){
-        for (int j = 0; j < cols; j++){
-            board[i][j].reset();
-        }
-    }
-}
-
-void resize_board(int rows, int cols, vector<vector<Cell>>& board){
-    // Resize the outer vector to the new number of rows
-    board.resize(rows);
-
-    // Resize each inner vector to the new number of columns
-    for (int i = 0; i < rows; ++i) {
-        board[i].resize(cols);
-    }
-    reset_board(rows, cols, board);
-}
-
-void randomize_board(int rows, int cols, float bomb_ratio, vector<vector<Cell>>& board){
-    reset_board(rows, cols, board);
-    int bombs_to_make = rows * cols * bomb_ratio;
-    vector<pair<int, int>> pairs;
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            pairs.push_back({i, j});
-        }
-    }
-
-    random_device rd;
-    mt19937 g(rd());
-    shuffle(pairs.begin(), pairs.end(), g);
-
-    for(int i = 0; i < bombs_to_make; i++){
-        board[pairs[i].first][pairs[i].second].set_bomb();
-        for(int j = pairs[i].first - 1; j <= pairs[i].first + 1; j++){
-            for(int k = pairs[i].second - 1; k <= pairs[i].second + 1; k++){
-                if(0 <= j && j <= rows - 1 && 0 <= k && k <= cols - 1){
-                    board[j][k].inc_content();
-                }
-            }
-        }
-    }
-}
-
-void print_board(int rows, int cols, vector<vector<Cell>>& board){
-    for (int i = 0; i < rows; i++){
-        for (int j = 0; j < cols; j++){
-            if(j != cols - 1){
-                cout << "[" << board[i][j].get_content() << "] ";
-            }
-            else{
-                cout << "[" << board[i][j].get_content() << "]" << endl;
-            }
-        }
-    }
-}
 
 int main(){    
     //set up "global" variables
     string state = "MENU";
 
-    int rows = 10;
-    int cols = 10;
-    float bomb_ratio = .10;
+    Board board;
+    board.print_board();
+    board.randomize_board();
+    cout << endl;
+    board.print_board();
 
-    vector<vector<Cell>> board(rows, vector<Cell>(cols, 0));
-
-    randomize_board(rows, cols, bomb_ratio, board);
-    print_board(rows, cols, board);
 
     //set up game board for default settings
 
